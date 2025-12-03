2d7c756fcb2ebce5a7dff1aa9e8bc8e7e75f565a
Guga Akopashvili <guga.akopashvili1711@gmail.com>
2025-12-03 15:07:47 +0400

    bugfix/adding-cors
    adding cors to backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,18 @@
 import express from "express";
+import cors from "cors";
 import dbConfig from "./config/db.config.js";
 import authRouter from "./auth/auth.controller.js";
 import productsRouter from "./products/products.controller.js";
 
 const app = express();
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use("/auth", authRouter);
 app.use("/products", productsRouter);
